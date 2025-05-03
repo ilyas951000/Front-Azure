153968469c5f7d32eecd6b9e732150413cc88128
ilyas <ilyas.salek@gmail.com>
2025-05-03 11:49:54 +0200

    nex version

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-// src/App.jsx
-
 import { useEffect, useState } from "react";
 
 function App() {
   const [message, setMessage] = useState("");
 
-  // Détection automatique de l'environnement
   const apiUrl = import.meta.env.PROD
     ? "https://back-projet-dpccajgja8c2f3df.francecentral-01.azurewebsites.net/api/hello"
-    : "/api/hello"; // proxy local
+    : "/api/hello";
 
   useEffect(() => {
     fetch(apiUrl)
       .then((res) => res.json())
       .then((data) => setMessage(data.message))
       .catch((err) => console.error("Erreur lors de la récupération du message :", err));
-  }, []);
+  }, [apiUrl]); // ✅ Ajout de apiUrl dans le tableau de dépendances
 
   return (
     <div style={{ padding: "2rem", fontFamily: "Arial" }}>
